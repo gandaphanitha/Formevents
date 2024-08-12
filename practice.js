c15bbc991f0d76291233e0b54c5938849a7f7fc0
gandaphanitha <161686624+gandaphanitha@users.noreply.github.com>
2024-08-12 10:27:39 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,15 @@
+let fruits=['apple','banana',"orange"]
+// console.log(typeof(fruits))
+// console.log(Array.isArray(fruits))
+// update array elements
+// fruits[2]="melon"
+// console.log(fruits)
+console.log(fruits.length)
+
+
+
+
+
+
+
+
